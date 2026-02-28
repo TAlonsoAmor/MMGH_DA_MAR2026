3b31211d0d1888ae38a1d266621939a49e6877c6
TAlonsoAmor <t.alonsoamor@gmail.com>
2026-02-28 21:41:30 +0000

    initial set up of project #1

---
+++ .Rprofile
@@ -0,0 +1,5 @@
+if (requireNamespace("renv", quietly = TRUE)) {
+  if (file.exists("renv/activate.R")) {
+    source("renv/activate.R")
+  }
+}
---
+++ libraries.R
@@ -0,0 +1,9 @@
+# This file is only meant for the setup of the R environment using renv
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(readxl)
+library(lubridate)
+library(janitor)
+library(forcats)
+library(stringr)
